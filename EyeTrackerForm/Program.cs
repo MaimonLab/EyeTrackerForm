be5412dbbd6e531d0fc4f644bc5197de1f6d0778
Kris Fonselius <kfonselius@rockefeller.edu>
2019-09-03 12:37:48 -0400

    fix main loop

--- EyeTrackerForm/Program.cs
+++ EyeTrackerForm/Program.cs
@@ -25,71 +25,56 @@ namespace EyeTrackerForm
 
 
 
-            GUIFactory mFactory = new GUIFactory();
-            CameraSelectionWindow window = mFactory.GetCameraSelectionWindow();
-            window.Height = 450.0;
-            SpinnakerNET.GUI.CameraSelectionWindow.DeviceEventArgs retval = window.ShowModal(false);
-            try
-            {
-                if (retval != null)
-                {
-                    retval.Camera.Init();
-                    Thread.Sleep(2500);
-                    INodeMap nodeMap = retval.Camera.GetNodeMap();
-                    retval.Camera.TriggerSource.Value = TriggerSourceEnums.Line2.ToString();
-                    retval.Camera.TriggerActivation.Value = TriggerActivationEnums.RisingEdge.ToString();
-                    retval.Camera.TriggerOverlap.Value = TriggerOverlapEnums.ReadOut.ToString();
-                    retval.Camera.ExposureMode.Value = ExposureModeEnums.Timed.ToString();
-                    retval.Camera.ExposureAuto.Value = ExposureAutoEnums.Off.ToString();
-                    IFloat iExposureTime = nodeMap.GetNode<IFloat>("ExposureTime");
-                    iExposureTime.Value = 19755.0;
-                    retval.Camera.TriggerMode.Value = TriggerModeEnums.On.ToString();
-                    Console.WriteLine("camera updated");
-
-                }
-                else
-                {
-                    Console.WriteLine("no camera selected");
-
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("error configuring camera");
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            //GUIFactory mFactory = new GUIFactory();
+            //CameraSelectionWindow window = mFactory.GetCameraSelectionWindow();
+            //window.Height = 450.0;
+            //SpinnakerNET.GUI.CameraSelectionWindow.DeviceEventArgs retval = window.ShowModal(false);
             //try
             //{
-            //    Application.EnableVisualStyles();
-            //    Application.SetCompatibleTextRenderingDefault(false);
-            //    EyeTrackerController controller = new EyeTrackerController();
-            //    Form1 thisForm = new Form1(controller);
-            //    controller.AddForm(thisForm);
-            //    //throw new Exception();
-            //    Application.Run(thisForm);
+            //    if (retval != null)
+            //    {
+            //        retval.Camera.Init();
+            //        Thread.Sleep(2500);
+            //        INodeMap nodeMap = retval.Camera.GetNodeMap();
+            //        retval.Camera.TriggerSource.Value = TriggerSourceEnums.Line2.ToString();
+            //        retval.Camera.TriggerActivation.Value = TriggerActivationEnums.RisingEdge.ToString();
+            //        retval.Camera.TriggerOverlap.Value = TriggerOverlapEnums.ReadOut.ToString();
+            //        retval.Camera.ExposureMode.Value = ExposureModeEnums.Timed.ToString();
+            //        retval.Camera.ExposureAuto.Value = ExposureAutoEnums.Off.ToString();
+            //        IFloat iExposureTime = nodeMap.GetNode<IFloat>("ExposureTime");
+            //        iExposureTime.Value = 19755.0;
+            //        retval.Camera.TriggerMode.Value = TriggerModeEnums.On.ToString();
+            //        Console.WriteLine("camera updated");
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("no camera selected");
+
+            //    }
             //}
             //catch (Exception e)
             //{
-            //    MessageBox.Show(e.Message + "/n" + e.StackTrace, "Error",
-            //       MessageBoxButtons.OK, MessageBoxIcon.Error);
-
+            //    Console.WriteLine("error configuring camera");
             //}
+
+
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                EyeTrackerController controller = new EyeTrackerController();
+                Form1 thisForm = new Form1(controller);
+                controller.AddForm(thisForm);
+                //throw new Exception();
+                Application.Run(thisForm);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message + "/n" + e.StackTrace, "Error",
+                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
         }
     }
 }
